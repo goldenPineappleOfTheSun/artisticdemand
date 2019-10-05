0a142764ef6d1dbf073a5406e7cfa8d3a7fb10e7
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2019-10-05 20:55:19 +0500

    --

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const $ = require('jquery');
 var app = express();
+var cors = require('cors');
 
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'dist')))
-  .get('/renderapitoken', (req, res) => {
-    res.send(req.query.code);
+  .get('/vkapitoken', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ 
+      code: req.query.code 
+    }));
+  })
+  .get('/vkapiauth', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(
+      { 
+        access_token: req.query.access_token, 
+        expires_in: req.query.expires_in, 
+        user_id: req.query.user_id
+      }));
   })
   .get('*', (req, res) => {
   	console.log('=)');
