6b351a0e8b229fc631c06f434e7991fdc8a0ff44
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2019-11-27 21:06:50 +0500

    Settle down the configuration

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const request = require('request');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var app = express();
-const { Pool } = require('pg');;
-const bodyParser = require('body-parser')
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
 const config = require('./config');
+let app = express();
+
+// vk app id
+const vkappid = config.vk.appid;
+// secret key for vk app
+const secretkey = process.env.VK_API_KEY;
+// connection to postgreg
+const pool = new Pool({
+    connectionString: config.pg.connectionString,
+    ssl: true
+});
 
 app
     .use(express.static(path.join(__dirname, 'public/')))
     .use(bodyParser.json())
     .set('views', './public')
     .set('view engine', 'pug')
+    .get('/ping', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query(`select * from ping`);    
+            res.send(`"${result.rows[0].ping}"`);
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }   
+    })
     .get('*', (req, res) => {
         res.send('hello!');
     })
