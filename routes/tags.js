302c75e003cffb8c53cbd46e56ca011b7fef9e15
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2019-12-10 20:53:50 +0500

    Add post request to put multiple tags

--- routes/tags.js
+++ routes/tags.js
@@ -65,6 +65,69 @@ router.put('/', async (req, res) => {
 });
 
 /**
+ * Puts multiple tags the base (max 50)
+ * @throws {400 No secret key} If internalsecret param is not correct
+ * @throws {400 No array} If array presented
+ * @throws {400 Too many} If array is bigger than 50
+ * @throws {400 invalid array} If some values in array is not right
+ */
+router.post('/', async (req, res) => {
+    // inputs and contracts
+    if (req.body.internalsecret !== process.env.SECRET) {
+        return res.status(400).send({ error: 'Sorry but you need to specify the secret key' });
+    }
+
+    let arr = req.body.arr;
+
+    if (arr === undefined) {
+        return res.status(400).send({ error: 'Need to specify array "arr"' });
+    }
+
+    if (arr.length === 0) {
+        return res.status(200).send('No rows to put!');
+    }
+
+    if (arr.length > 50) {
+        return res.status(400).send({ error: 'Too many rows' });
+    }
+
+    let names = [];
+    let defvalues = [];
+    let titles = [];
+    try {
+        names = arr.map(x => {
+            if (typeof x.name !== 'string') {
+                throw new Error('Need to specify name for all the tags');
+            } else {
+                return x.name;
+            }
+        });
+        defvalues = arr.map(x => x.defvalue || false);
+        titles = arr.map(x => x.title || x.name);
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+
+    // query
+    let client;
+    try {
+        client = await pool.connect();
+        await client.query(`INSERT INTO tag (name, defvalue, title)
+            VALUES (
+                UNNEST(ARRAY[${names.map(x => `'${x}'`).join(',')}]), 
+                UNNEST(ARRAY[${defvalues.join(',')}]), 
+                UNNEST(ARRAY[${titles.map(x => `'${x}'`).join(',')}])
+            );`);
+        return res.sendStatus(200);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({ error: err });
+    } finally {
+        client.end();
+    }
+});
+
+/**
  * Loads all tags using paging. Max page size is 200
  * @throws {400 Pagesize} If pagesize is smaller than 1
  * @throws {400 Pagesize} If pagesize is bigger than 200
